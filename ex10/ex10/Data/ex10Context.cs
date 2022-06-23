0783fc1b1865c7c462fd8ef8261ae027e0fb5153
s22229 <s22229@pjwstk.edu.pl>
2022-06-23 02:24:10 +0200

    final commit

---
+++ ex10/ex10/Data/ex10Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ex10.Models;
+
+namespace ex10.Data
+{
+    public class ex10Context : DbContext
+    {
+        public ex10Context (DbContextOptions<ex10Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ex10.Models.Movie> Movie { get; set; }
+    }
+}
